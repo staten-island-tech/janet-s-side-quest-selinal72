b0504aeae77cfec9e5c65a210647e9b702cde653
selinal72 <selinal72@nycstudents.net>
2025-03-11 10:28:30 -0400

    working on 4

--- app.py
+++ app.py
@@ -10,21 +10,49 @@ def csv_to_list(file_path):
 
     return data_list
 
-
 file_path = "SalesData.csv"  
 data = csv_to_list(file_path)
 
 
-avgs = []
+# 1) 
+""" avgs = []
 for row in data[1:]:
     stores = row[0]
     sales = list(map(int, row[1:]))
     avg_sales = sum(sales) / len(sales)
     avgs.append((stores, avg_sales))
+print(avgs) """
+
+# 2) 
+
+
+# 3)
+""" avgs = []
+for row in data[1:]:
+    sales = list(map(int, row[1:]))
+    avg_sales = sum(sales) / len(sales)
+    avgs.append(avg_sales)
+for x in avgs:
+    all_stores = sum(avgs) / len(avgs)
+print(all_stores) """
+
+# 4) 
+avgs = []
+danger = []
+below = 0
+all_stores = 0
+for row in data[1:]:
+    stores = row[0]
+    sales = list(map(int, row[1:]))
+    avg_sales = sum(sales) / len(sales)
+    avgs.append(avg_sales)
+    all_stores = sum(avgs) / len(avgs)
+    below = all_stores * 0.8
+print("AVWERAGES")
 print(avgs)
-
-
-
-
-    
-
+print(all_stores, below)
+for x in avgs:
+    print(x)
+    if x < below:
+        danger.append((stores, x))
+print(danger)
